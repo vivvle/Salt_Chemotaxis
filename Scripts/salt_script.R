c91bc640da59d9c97963738ffafe65d7fcd0be35
vivvle <vivianvnle@gmail.com>
2022-04-27 19:01:41 -0700

    updating script v2

--- Scripts/salt_script.R
+++ Scripts/salt_script.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(ggplot2)
 library(here)
 library(palettetown)
+library(rstatix)
 
 #### load data ####
 salt <- read.csv(here("Data", "salt_CTX_data.csv"))
@@ -25,8 +26,12 @@ salt$participation <- ((salt$total/(salt$total + salt$Dispersed))) #calculating 
 view(salt)
 
 salt_data <- salt %>%
-  relocate("total", .before = "Dispersed") %>% #moved total column before dispersed
-  write.csv(here("Output","salt_CI.csv")) #saved as an output with calculations
+  relocate("total", .before = "Dispersed") #moved total column before dispersed
+view(salt_data)
+write.csv(here("Output","salt_CI.csv"))
+
+salt_data %>%
+  summarize(average = mean(CI))
 view(salt_data)
 
 #### data analysis for all genes ####
@@ -36,17 +41,20 @@ allgenes$participation <- ((allgenes$total)/(allgenes$total + allgenes$Dispersed
 view(allgenes)
 
 allgenes_data <- allgenes %>%
-  relocate("total", .before = "Dispersed") %>%
-  write.csv(here("Output", "allgenes_CI.csv"))
+  relocate("total", .before = "Dispersed")
 view(allgenes_data)
+write.csv(here("Output", "allgenes_CI.csv"))
 
 allgenes_CI <- allgenes_data %>%
   select(Strain, Salt, CI)
 view(allgenes_CI)
 
+# need to get the averages of each salt per strain
+
 
 #### plotting data ####
 p1 <- salt_data %>%
-  ggplot(mapping = aes(x = Strain, y = CI, group = Salt)) +
-  geom_point()
+  ggplot(mapping = aes(x = Salt, y = CI, group = Strain, fill = Strain)) +
+  geom_col(position = "dodge")
 
+p1
